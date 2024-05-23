5baafcaebd8d58c9bede56d5b71317330c8c8158
Amit Gal <156659381+AGDevLab@users.noreply.github.com>
2024-05-23 12:46:39 +0300

    First Draft Limited Functionality

---
+++ js/game.js
@@ -0,0 +1,230 @@
+'use strict'
+
+// In Progress, Limited functionality
+
+var gBoard, numOfNegMines, mine
+
+const MINE = '*'
+
+const gLevel = {
+  SIZE: 4,
+  MINES: 2,
+}
+
+var gGame = {
+  isOn: false,
+  shownCount: 0,
+  markedCount: 0,
+  secsPassed: 0,
+}
+
+/* The Model:
+
+gBoard – A Matrix
+containing cell objects:
+Each cell: {
+ minesAroundCount: 4,
+ isShown: false,
+ isMine: false,
+ isMarked: true
+}
+*/
+
+/*
+This is an object by which the
+board size is set (in this case:
+4x4 board and how many mines
+to place)
+
+gLevel = {
+ SIZE: 4,
+ MINES: 2
+}
+*/
+
+/*
+This is an object in which you
+can keep and update the
+current game state:
+isOn: Boolean, when true we
+let the user play
+shownCount: How many cells
+are shown
+markedCount: How many cells
+are marked (with a flag)
+secsPassed: How many seconds
+passed 
+
+gGame = {
+ isOn: false,
+ shownCount: 0,
+ markedCount: 0,
+ secsPassed: 0
+}
+*/
+
+// This is called when page loads
+function onInit() {
+  console.log('onInit')
+  gBoard = buildBoard()
+  console.log('gBoard', gBoard)
+  renderBoard(gBoard)
+}
+
+// Builds the board Set the mines Call setMinesNegsCount() Return the created board
+// step1.1 done
+function buildBoard() {
+  const board = createMat(gLevel.SIZE, gLevel.SIZE)
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      board[i][j] = {
+        minesAroundCount: numOfNegMines,
+        isShown: false,
+        isMine: false,
+        isMarked: true,
+      }
+      // if (
+      //   i === 0 ||
+      //   i === board.length - 1 ||
+      //   j === 0 ||
+      //   j === board[0].length - 1
+      // )
+      // if (
+      //   (i === 0 && j === board.length - 1) ||
+      //   (i === 1 && j === board.length - 1) ||
+      //   (j === 0 && i === board[0].length - 1)
+      // ) {
+      // board[i][j].isMine = true
+      // }
+    }
+  }
+  board[0][3].isMine = true
+  board[3][0].isMine = true
+
+  return board
+}
+
+// step1.3 done
+function renderBoard(board) {
+  var strHTML = ''
+  for (var i = 0; i < board.length; i++) {
+    strHTML += '<tr>'
+    for (var j = 0; j < board[0].length; j++) {
+      const currCell = board[i][j]
+      numOfNegMines = setMinesNegsCount(i, j, board)
+      currCell.minesAroundCount = numOfNegMines
+      // console.log(currCell, numOfNegMines)
+
+      var cellClass = getClassName({ i: i, j: j }) // cell-i-j
+
+      if (currCell.isMine) cellClass += ' mine'
+      // else if (currCell.type === WALL) cellClass += ' wall'
+
+      strHTML += `<td class="cell ${cellClass}" onclick="onCellClicked(this,${i},${j})" ><span>`
+
+      if (currCell.isMine) {
+        strHTML += MINE
+      } else if (!currCell.mine) {
+        strHTML += currCell.minesAroundCount
+      }
+
+      strHTML += '</span></td>'
+    }
+    strHTML += '</tr>'
+  }
+
+  const elBoard = document.querySelector('.board')
+  elBoard.innerHTML = strHTML
+}
+
+// Convert a location object {i, j} to a selector and render a value in that element
+function renderCell(location, value) {
+  const cellSelector = '.' + getClassName(location) // '.cell-1-1'
+  const elCell = document.querySelector(cellSelector) // <td></td>
+  elCell.innerHTML = value
+} // to be used later
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+  const cellClass = 'cell-' + location.i + '-' + location.j
+  return cellClass
+} // step1.3 done
+
+function setMinesNegsCount(cellI, cellJ, board) {
+  // 7,0
+  var minesCount = 0
+  for (var i = cellI - 1; i <= cellI + 1; i++) {
+    if (i < 0 || i >= board.length) continue
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      const currCell = board[i][j]
+
+      if (j < 0 || j >= board[i].length) continue
+      if (i === cellI && j === cellJ) continue
+
+      if (currCell.isMine) {
+        minesCount++
+      }
+    }
+  }
+  return minesCount
+}
+
+// ---- to be finished... only basic functionality working
+// TODO: Called when a cell is clicked
+function onCellClicked(elCell, cellI, cellJ) {
+  console.log(elCell)
+  console.log(elCell.innerText)
+
+  // console.log('elCell:', elCell)
+  if (elCell.innerText === '0') {
+    // if (elCell.classList.contains('occupied')) {
+    // if (gBoard[cellI][cellJ] === LIFE) {
+
+    //   // Update the Model:
+    gBoard[cellI][cellJ].isShown = true
+
+    //   // Update the Dom:
+    elCell.innerText = ''
+    console.log(elCell.innerText)
+
+    // showCell(cellI, cellJ)/
+  }
+}
+
+// TODO: Called when a cell is rightclicked See how you can hide the context menu on right click
+function onCellMarked(elCell) {}
+
+// TODO: Game ends when all mines are marked, and all the other cells are shown
+function checkGameOver() {}
+
+/*
+When user clicks a cell with no mines around, we need to open not only that cell, but also its
+neighbors. NOTE: start with a basic implementation that only opens the non-mine 1st degree
+neighbors.
+BONUS: if you have the time later, try to work more like the real algorithm (see description
+at the Bonuses section below)
+*/
+// TODO:
+function expandShown(board, elCell, i, j) {}
+
+function createMat(ROWS, COLS) {
+  const mat = []
+  for (var i = 0; i < ROWS; i++) {
+    const row = []
+    for (var j = 0; j < COLS; j++) {
+      row.push('')
+    }
+    mat.push(row)
+  }
+  return mat
+}
+
+// function hideCell(cell) {
+//   const cell = cell.querySelector('span')
+//   cell.classList.add('hidden')
+// }
+
+// function showCell() {
+//   const cell = cell.querySelector('span')
+//   cell.classList.remove('hidden')
+// }
---
+++ js/util.js
@@ -0,0 +1,299 @@
+function countNegs(cellI, cellJ, mat) {
+  // 7,0
+  var negsCount = 0
+  for (var i = cellI - 1; i <= cellI + 1; i++) {
+    if (i < 0 || i >= mat.length) continue
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      if (j < 0 || j >= mat[i].length) continue
+      if (i === cellI && j === cellJ) continue
+
+      if (mat[i][j] === LIFE) negsCount++
+    }
+  }
+  return negsCount
+}
+
+function createBoard() {
+  var board = []
+  for (var i = 0; i < 8; i++) {
+    board.push([])
+    for (var j = 0; j < 8; j++) {
+      board[i][j] = Math.random() > 0.5 ? LIFE : ''
+    }
+  }
+  return board
+}
+
+function buildBoard() {
+  // Create the Matrix 10 * 12
+  const board = createMat(10, 12)
+  // Put FLOOR everywhere and WALL at edges
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      board[i][j] = { type: FLOOR, gameElement: null }
+      if (
+        i === 0 ||
+        i === board.length - 1 ||
+        j === 0 ||
+        j === board[0].length - 1
+      ) {
+        board[i][j].type = WALL
+      }
+    }
+  }
+
+  // Place the gamer and two balls
+  board[gGamerPos.i][gGamerPos.j].gameElement = GAMER
+
+  board[2][4].gameElement = BALL
+  board[7][6].gameElement = BALL
+
+  console.log(board)
+  return board
+}
+
+// Render the board to an HTML table
+function renderBoard(board) {
+  var strHTML = ''
+  for (var i = 0; i < board.length; i++) {
+    strHTML += '<tr>'
+    for (var j = 0; j < board[0].length; j++) {
+      const currCell = board[i][j] // {type,gameElement}
+
+      var cellClass = getClassName({ i: i, j: j }) // 'cell-0-0'
+
+      if (currCell.type === FLOOR) cellClass += ' floor' // 'cell-0-0 floor'
+      else if (currCell.type === WALL) cellClass += ' wall' // 'cell-0-0 wall'
+
+      strHTML +=
+        '<td class="cell ' +
+        cellClass +
+        '"  onclick="moveTo(' +
+        i +
+        ',' +
+        j +
+        ')" >'
+
+      if (currCell.gameElement === GAMER) {
+        strHTML += GAMER_IMG
+      } else if (currCell.gameElement === BALL) {
+        strHTML += BALL_IMG
+      }
+
+      strHTML += '</td>'
+    }
+    strHTML += '</tr>'
+  }
+
+  const elBoard = document.querySelector('.board')
+  elBoard.innerHTML = strHTML
+}
+
+// Move the player to a specific location
+function moveTo(i, j) {
+  console.log('i, j:', i, j)
+
+  const targetCell = gBoard[i][j]
+  if (targetCell.type === WALL) return
+
+  // Calculate distance to make sure we are moving to a neighbor cell
+  const iAbsDiff = Math.abs(i - gGamerPos.i) // 1 ,2..
+  const jAbsDiff = Math.abs(j - gGamerPos.j) // 1 ,7...
+
+  // If the clicked Cell is one of the four allowed
+  if (
+    (iAbsDiff === 1 && jAbsDiff === 0) ||
+    (jAbsDiff === 1 && iAbsDiff === 0)
+  ) {
+    console.log('MOVE')
+    if (targetCell.gameElement === BALL) {
+      console.log('Collecting!')
+    }
+
+    // Move the gamer
+    // Moving from current position:
+    // Model:
+    gBoard[gGamerPos.i][gGamerPos.j].gameElement = null
+
+    // Dom:
+    renderCell(gGamerPos, '')
+
+    // Moving to selected position:
+    // Model:
+    gBoard[i][j].gameElement = GAMER
+    gGamerPos.i = i
+    gGamerPos.j = j
+
+    // Dom:
+    renderCell(gGamerPos, GAMER_IMG)
+  } else console.log('TOO FAR', iAbsDiff, jAbsDiff)
+}
+
+// Convert a location object {i, j} to a selector and render a value in that element
+function renderCell(location, value) {
+  const cellSelector = '.' + getClassName(location)
+  const elCell = document.querySelector(cellSelector)
+  elCell.innerHTML = value
+}
+
+// Move the player by keyboard arrows
+function onHandleKey(event) {
+  // console.log('event:', event)
+  const i = gGamerPos.i // 2
+  const j = gGamerPos.j // 9
+
+  switch (event.key) {
+    case 'ArrowLeft':
+      moveTo(i, j - 1)
+      break
+    case 'ArrowRight':
+      moveTo(i, j + 1)
+      break
+    case 'ArrowUp':
+      moveTo(i - 1, j)
+      break
+    case 'ArrowDown':
+      moveTo(i + 1, j)
+      break
+  }
+}
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+  // {i:2,j:4}
+  const cellClass = `cell-${location.i}-${location.j}` // 'cell-2-4'
+  return cellClass
+}
+
+// ----- chess
+
+function cellClicked(elCell) {
+  // console.log('elCell:', elCell)
+
+  // if the target is marked - move the piece!
+
+  if (elCell.classList.contains('mark')) {
+    // console.log('move')
+    movePiece(gSelectedElCell, elCell)
+    cleanBoard()
+    return
+  }
+
+  cleanBoard()
+
+  elCell.classList.add('selected')
+
+  // Saving in a global variable
+  gSelectedElCell = elCell
+
+  // console.log('elCell.id:', elCell.id) // 'cell-1-2'
+  const cellCoord = getCellCoord(elCell.id) // {i:1,j:2}
+  const piece = gBoard[cellCoord.i][cellCoord.j]
+  console.log('piece:', piece)
+  // Another way
+  // const piece = elCell.innerText
+
+  var possibleCoords = []
+  switch (piece) {
+    case ROOK_BLACK:
+    case ROOK_WHITE:
+      possibleCoords = getAllPossibleCoordsRook(cellCoord)
+      break
+    case BISHOP_BLACK:
+    case BISHOP_WHITE:
+      possibleCoords = getAllPossibleCoordsBishop(cellCoord)
+      break
+    case KNIGHT_BLACK:
+    case KNIGHT_WHITE:
+      possibleCoords = getAllPossibleCoordsKnight(cellCoord)
+      break
+    case PAWN_BLACK:
+    case PAWN_WHITE:
+      possibleCoords = getAllPossibleCoordsPawn(cellCoord, piece === PAWN_WHITE)
+      break
+  }
+  markCells(possibleCoords)
+}
+
+function cleanBoard() {
+  const elTds = document.querySelectorAll('.mark, .selected')
+  for (var i = 0; i < elTds.length; i++) {
+    elTds[i].classList.remove('mark', 'selected')
+  }
+}
+
+function getSelector(coord) {
+  return `#cell-${coord.i}-${coord.j}`
+}
+
+function isEmptyCell(coord) {
+  // return gBoard[coord.i][coord.j]  === ''
+  return !gBoard[coord.i][coord.j]
+}
+
+function getAllPossibleCoordsPawn(pieceCoord, isWhite) {
+  // handle PAWN use isEmptyCell()
+  // var res = [{i:2,j:0},{i:3,j:0}]
+  var res = []
+
+  var diff = isWhite ? -1 : 1
+  var nextCoord = { i: pieceCoord.i + diff, j: pieceCoord.j }
+  if (isEmptyCell(nextCoord)) res.push(nextCoord)
+  else return res
+
+  if ((!isWhite && pieceCoord.i === 1) || (isWhite && pieceCoord.i === 6)) {
+    diff *= 2
+    nextCoord = { i: pieceCoord.i + diff, j: pieceCoord.j }
+    if (isEmptyCell(nextCoord)) res.push(nextCoord)
+  }
+
+  return res
+}
+
+// ----------
+
+function cellClicked(elCell, cellI, cellJ) {
+  // console.log('elCell:', elCell)
+  // if (elCell.innerText === LIFE) {
+  // if (elCell.classList.contains('occupied')) {
+  if (gBoard[cellI][cellJ] === LIFE) {
+    // Update the Model:
+    gBoard[cellI][cellJ] = SUPER_LIFE
+
+    // Update the Dom:
+    elCell.innerText = SUPER_LIFE
+
+    blowUpNegs(cellI, cellJ)
+  }
+}
+
+function chooseLevelSize(elBtn) {
+  // console.log('elBtn.dataset:', elBtn.dataset)
+  var size = +elBtn.dataset.size
+  // console.log('size:', size)
+  gSize = size
+  onInit()
+}
+
+function copyMat(mat) {
+  var newMat = []
+  for (var i = 0; i < mat.length; i++) {
+    newMat[i] = []
+    for (var j = 0; j < mat[0].length; j++) {
+      newMat[i][j] = mat[i][j]
+    }
+  }
+  return newMat
+}
+
+function createMat(ROWS, COLS) {
+  const mat = []
+  for (var i = 0; i < ROWS; i++) {
+    const row = []
+    for (var j = 0; j < COLS; j++) {
+      row.push('')
+    }
+    mat.push(row)
+  }
+  return mat
+}
